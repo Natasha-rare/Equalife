5139174ebc0feffd954d2dd1c4b6a4eea1847d9e
Tarasov Egor <tarasov.egor.yandex@gmail.com>
2021-07-26 15:51:02 +0300

    Добавлен тестовый объект Article

--- Equalife/Equalife/Controller/ArticleVC.swift
+++ Equalife/Equalife/Controller/ArticleVC.swift
@@ -7,26 +7,22 @@
 
 import UIKit
 
-
+//Текст, с помощью которого проверял скрол (его можно удалить как добавите подгрузку статьи)
 let content = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra venenatis mauris vitae egestas. In vel lectus ut massa rhoncus rhoncus. Aliquam consequat venenatis augue, vitae mollis nunc sodales vitae. Nullam condimentum iaculis nisi, vel convallis sem tincidunt vitae. Sed sit amet ligula sit amet augue rutrum congue sed a ligula. Aenean pharetra lobortis mauris, ut luctus est egestas ut. Ut viverra imperdiet pellentesque. In ut tristique ante.
     Vivamus magna mi, aliquam sit amet metus non, accumsan malesuada nisi. Nunc cursus sapien in sem porta, id molestie eros aliquam. Vivamus nibh ante, placerat id massa in, semper fringilla nisi. Sed fringilla purus orci, vel placerat felis feugiat eu. Nunc suscipit diam eu feugiat auctor. Integer ut elit quis lacus maximus feugiat. Integer congue massa dolor, ut pellentesque diam condimentum eu. Phasellus blandit ultrices euismod. Duis vulputate ligula felis, at ultrices est semper eget. Morbi vel rhoncus arcu, tempor scelerisque ex. Curabitur congue quam eget tellus consectetur dignissim. Mauris venenatis enim diam, eu gravida nibh elementum sit amet. Donec pulvinar tristique mauris vel rutrum. Donec ut ipsum id justo imperdiet varius.In elit ex, bibendum in purus placerat, imperdiet bibendum felis.
     Nulla varius ultrices felis quis lobortis. Donec vel finibus neque, id dapibus nunc. Maecenas et vulputate sem. Donec faucibus, tellus ullamcorper imperdiet pellentesque, ipsum eros gravida ante, vitae efficitur mauris nibh sit amet turpis. Suspendisse ut volutpat nisl. Sed ornare nibh eros, a pretium lacus dignissim at. Nam euismod tortor quis sem lacinia maximus. Aenean vulputate, nisi ut varius viverra, sapien nisl faucibus orci, quis iaculis turpis tellus sit amet odio. Praesent pulvinar dolor interdum sagittis fermentum.
 """
+
+// Тестовый объект Article
 let testArticle = Article(title: "TestArticle", contents: content, imagesURL: ["FlatLogo"], author: "Author", date: "22-07-2021", isSaved: false)
 
 class ArticleViewController: UIViewController{
+    
 
-
-//    var articleImageView = UIImageView()
-//    var articleTextView = UITextView()
     let screenWidth = UIScreen.screenWidth
     let screenHeight = UIScreen.screenHeight
-//    var article : Article
-//
-//    func previewArticle(article : Article){
-//        self.article = article
-//    }
+
     @IBOutlet weak var articleTextView: UITextView!
     @IBOutlet weak var articleImageView: UIImageView!
     @IBOutlet weak var StackView: UIStackView!
@@ -34,14 +30,15 @@ class ArticleViewController: UIViewController{
     @IBOutlet weak var articleDateLabel: UILabel!
     @IBOutlet weak var articleAurhorLabel: UILabel!
 
-    let logo = UIImage(named: "LogoFlat")
-    let author : String = "Author"
-    let date : String = "2020-20-20"
+    // TODO: Надо добавить загрузку фото из сети
+    let image = UIImage(named : testArticle.imagesURL[0])
+    let author : String = testArticle.author ?? " "
+    let date : String = testArticle.date
     
     override func viewDidLoad() {
         super.viewDidLoad()
         articleImageView.contentMode = .scaleAspectFit
-        articleImageView.image = logo
+        articleImageView.image = image
         articleImageView.isUserInteractionEnabled = false
 
         articleAurhorLabel.text = author
@@ -63,5 +60,3 @@ class ArticleViewController: UIViewController{
         //self.StackView.addSubview(articleTextView)
     }
 }
-
-
