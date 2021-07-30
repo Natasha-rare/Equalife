61db3794c8b89aed27017b3e03ea05748eeaec69
Tarasov Egor <tarasov.egor.yandex@gmail.com>
2021-07-30 20:34:48 +0300

    added geometry detection

--- Equalife/Equalife/View/ArticleView.swift
+++ Equalife/Equalife/View/ArticleView.swift
@@ -16,80 +16,104 @@ struct ArticleView: View {
     
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false, content: {
-            ZStack{
-
-                VStack{
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 20) {
-                                ForEach((0...article.imagesURL.count - 1), id: \.self) { imageIndex in
-//                                    Image()
-                                    KFImage(URL(string: article.imagesURL[imageIndex])!)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.3)
-                                }
-                            }
-                        }
-                        .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.3, alignment: .center)
-                        .overlay(
-                            ZStack(){
-                                LinearGradient(gradient:
-                                                    Gradient(
-                                                        colors: [
-                                                                Color(UIColor.black.withAlphaComponent(1)),
-                                                                Color(UIColor.black.withAlphaComponent(0.2)),
-                                                                Color(UIColor.white.withAlphaComponent(0.0))
-                                                        ]),
-                                                   startPoint: .bottom,
-                                                   endPoint: .center
-                                    )
-                                VStack{
-                                    Spacer()
-                                    HStack{
-                                        Text(article.author ??  " " )
-                                            .foregroundColor(.white)
-                                            .font(.title)
-                                            .fontWeight(.bold)
-                                            .padding([.leading, .trailing], 5)
-                                            
-                                            
-                                        Spacer()
-                                        Text(article.date)
-                                            .foregroundColor(.white)
-                                            .font(.title)
-                                            .fontWeight(.bold)
-                                            .padding([.leading, .trailing], 5)
-                                            
+            ScrollView(.vertical, showsIndicators: false, content: {
+                GeometryReader{ geometry in
+                ZStack{
+                    VStack{
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    ForEach((0...article.imagesURL.count - 1), id: \.self) { imageIndex in
+                                        KFImage(URL(string: article.imagesURL[imageIndex])!)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.3)
                                     }
                                 }
                             }
-                            
-                        )
-                    VStack{
-                        HStack{
-                            Text(article.title)
-                                .foregroundColor(Color(.black))
-                                .font(.title)
-                                .fontWeight(.semibold)
-                                .padding(.leading, 20)
+                            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.3, alignment: .center)
+                            .overlay(
+                                ZStack(){
+                                    LinearGradient(gradient:
+                                                        Gradient(
+                                                            colors: [
+                                                                    Color(UIColor.black.withAlphaComponent(1)),
+                                                                    Color(UIColor.black.withAlphaComponent(0.2)),
+                                                                    Color(UIColor.white.withAlphaComponent(0.0))
+                                                            ]),
+                                                       startPoint: .bottom,
+                                                       endPoint: .center
+                                        )
+                                    VStack{
+                                        Spacer()
+                                        HStack{
+                                            Text(article.author ??  " " )
+                                                .foregroundColor(.white)
+                                                .font(.title)
+                                                .fontWeight(.bold)
+                                                .padding([.leading, .trailing], 5)
+                                                
+                                                
+                                            Spacer()
+                                            Text("Top View \(geometry.frame(in: .global).midY)")
+                                                .foregroundColor(.white)
+                                                .font(.title)
+                                                .fontWeight(.bold)
+                                                .padding([.leading, .trailing], 5)
+                                                
+                                        }
+                                    }
+                                }
                                 
-                            Spacer()
+                            )
+                        VStack{
+                            HStack{
+                                Text(article.title)
+                                    .foregroundColor(Color(.black))
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                    .padding(.leading, 20)
+                                    
+                                Spacer()
+                            }
+                            Text(article.contents)
+                                .foregroundColor(Color(.black))
+                                .font(.body)
+                                .multilineTextAlignment(.leading)
+                                .padding([.leading, .trailing] , 20)
                         }
-                        Text(article.contents)
-                            .foregroundColor(Color(.black))
-                            .font(.body)
-                            .multilineTextAlignment(.leading)
-                            .padding([.leading, .trailing] , 20)
                     }
-//                    .background(
-//                        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)), Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))]), startPoint: .top, endPoint: .bottom)
-//                    )
                 }
-            }
-        }).ignoresSafeArea()
+                }
+                
+            })
+            .ignoresSafeArea()
     }
 }
+
+struct BackButton : View{
+    var body : some View{
+        Button(action: {
+            
+        }, label: {
+            Image(systemName: "arrowshape.turn.up.left.circle.fill")
+                .font(.largeTitle)
+                .foregroundColor(.black)
+                .frame(width: UIScreen.screenWidth * 0.08, height: UIScreen.screenHeight * 0.08)
+        })
+    }
+    
+}
+
+struct CustomNavigationView : View{
+    
+    var body: some View{
+        HStack{
+            Text("Назад")
+            Spacer()
+        }
+    }
+}
+
 
 // extension для получения размеров экрана
 extension UIScreen{
