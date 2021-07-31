67129c2a80c24cc8732d45e33664bbb1071401bf
Kostya Bunsberry <kostyabunsberry@macbook.local>
2021-07-31 17:05:56 +0700

    Добавил библиотеку для синхронизации

---
+++ Equalife/Pods/IceCream/IceCream/Classes/CKRecordConvertible.swift
@@ -0,0 +1,173 @@
+//
+//  Object+CKRecord.swift
+//  IceCream
+//
+//  Created by 蔡越 on 11/11/2017.
+//
+
+import Foundation
+import CloudKit
+import RealmSwift
+
+public protocol CKRecordConvertible {
+    static var recordType: String { get }
+    static var zoneID: CKRecordZone.ID { get }
+    static var databaseScope: CKDatabase.Scope { get }
+    
+    var recordID: CKRecord.ID { get }
+    var record: CKRecord { get }
+
+    var isDeleted: Bool { get }
+}
+
+extension CKRecordConvertible where Self: Object {
+    
+    public static var databaseScope: CKDatabase.Scope {
+        return .private
+    }
+    
+    public static var recordType: String {
+        return className()
+    }
+    
+    public static var zoneID: CKRecordZone.ID {
+        switch Self.databaseScope {
+        case .private:
+            return CKRecordZone.ID(zoneName: "\(recordType)sZone", ownerName: CKCurrentUserDefaultName)
+        case .public:
+            return CKRecordZone.default().zoneID
+        default:
+            fatalError("Shared Database is not supported now")
+        }
+    }
+    
+    /// recordName : this is the unique identifier for the record, used to locate records on the database. We can create our own ID or leave it to CloudKit to generate a random UUID.
+    /// For more: https://medium.com/@guilhermerambo/synchronizing-data-with-cloudkit-94c6246a3fda
+    public var recordID: CKRecord.ID {
+        guard let sharedSchema = Self.sharedSchema() else {
+            fatalError("No schema settled. Go to Realm Community to seek more help.")
+        }
+        
+        guard let primaryKeyProperty = sharedSchema.primaryKeyProperty else {
+            fatalError("You should set a primary key on your Realm object")
+        }
+        
+        switch primaryKeyProperty.type {
+        case .string:
+            if let primaryValueString = self[primaryKeyProperty.name] as? String {
+                // For more: https://developer.apple.com/documentation/cloudkit/ckrecord/id/1500975-init
+                assert(primaryValueString.allSatisfy({ $0.isASCII }), "Primary value for CKRecord name must contain only ASCII characters")
+                assert(primaryValueString.count <= 255, "Primary value for CKRecord name must not exceed 255 characters")
+                assert(!primaryValueString.starts(with: "_"), "Primary value for CKRecord name must not start with an underscore")
+                return CKRecord.ID(recordName: primaryValueString, zoneID: Self.zoneID)
+            } else {
+                assertionFailure("\(primaryKeyProperty.name)'s value should be String type")
+            }
+        case .int:
+            if let primaryValueInt = self[primaryKeyProperty.name] as? Int {
+                return CKRecord.ID(recordName: "\(primaryValueInt)", zoneID: Self.zoneID)
+            } else {
+                assertionFailure("\(primaryKeyProperty.name)'s value should be Int type")
+            }
+        default:
+            assertionFailure("Primary key should be String or Int")
+        }
+        fatalError("Should have a reasonable recordID")
+    }
+    
+    // Simultaneously init CKRecord with zoneID and recordID, thanks to this guy: https://stackoverflow.com/questions/45429133/how-to-initialize-ckrecord-with-both-zoneid-and-recordid
+    public var record: CKRecord {
+        let r = CKRecord(recordType: Self.recordType, recordID: recordID)
+        let properties = objectSchema.properties
+        for prop in properties {
+            
+            let item = self[prop.name]
+            
+            if prop.isArray {
+                switch prop.type {
+                case .int:
+                    guard let list = item as? List<Int>, !list.isEmpty else { break }
+                    let array = Array(list)
+                    r[prop.name] = array as CKRecordValue
+                case .string:
+                    guard let list = item as? List<String>, !list.isEmpty else { break }
+                    let array = Array(list)
+                    r[prop.name] = array as CKRecordValue
+                case .bool:
+                    guard let list = item as? List<Bool>, !list.isEmpty else { break }
+                    let array = Array(list)
+                    r[prop.name] = array as CKRecordValue
+                case .float:
+                    guard let list = item as? List<Float>, !list.isEmpty else { break }
+                    let array = Array(list)
+                    r[prop.name] = array as CKRecordValue
+                case .double:
+                    guard let list = item as? List<Double>, !list.isEmpty else { break }
+                    let array = Array(list)
+                    r[prop.name] = array as CKRecordValue
+                case .data:
+                    guard let list = item as? List<Data>, !list.isEmpty else { break }
+                    let array = Array(list)
+                    r[prop.name] = array as CKRecordValue
+                case .date:
+                    guard let list = item as? List<Date>, !list.isEmpty else { break }
+                    let array = Array(list)
+                    r[prop.name] = array as CKRecordValue
+                case .object:
+                    /// We may get List<Cat> here
+                    /// The item cannot be casted as List<Object>
+                    /// It can be casted at a low-level type `ListBase`
+                    guard let list = item as? ListBase, list.count > 0 else { break }
+                    var referenceArray = [CKRecord.Reference]()
+                    let wrappedArray = list._rlmArray
+                    for index in 0..<wrappedArray.count {
+                        guard let object = wrappedArray[index] as? Object, let primaryKey = object.objectSchema.primaryKeyProperty?.name else { continue }
+                        switch object.objectSchema.primaryKeyProperty?.type {
+                        case .string:
+                            if let primaryValueString = object[primaryKey] as? String, let obj = object as? CKRecordConvertible, !obj.isDeleted {
+                                let referenceZoneID = CKRecordZone.ID(zoneName: "\(object.objectSchema.className)sZone", ownerName: CKCurrentUserDefaultName)
+                                referenceArray.append(CKRecord.Reference(recordID: CKRecord.ID(recordName: primaryValueString, zoneID: referenceZoneID), action: .none))
+                            }
+                        case .int:
+                            if let primaryValueInt = object[primaryKey] as? Int, let obj = object as? CKRecordConvertible, !obj.isDeleted {
+                                let referenceZoneID = CKRecordZone.ID(zoneName: "\(object.objectSchema.className)sZone", ownerName: CKCurrentUserDefaultName)
+                                referenceArray.append(CKRecord.Reference(recordID: CKRecord.ID(recordName: "\(primaryValueInt)", zoneID: referenceZoneID), action: .none))
+                            }
+                        default:
+                            break
+                        }
+                    }
+                    r[prop.name] = referenceArray as CKRecordValue
+                default:
+                    break
+                    /// Other inner types of List is not supported yet
+                }
+                continue
+            }
+            
+            switch prop.type {
+            case .int, .string, .bool, .date, .float, .double, .data:
+                r[prop.name] = item as? CKRecordValue
+            case .object:
+                guard let objectName = prop.objectClassName else { break }
+                // If object is CreamAsset, set record with its wrapped CKAsset value
+                if objectName == CreamAsset.className(), let creamAsset = item as? CreamAsset {
+                    r[prop.name] = creamAsset.asset
+                } else if let owner = item as? CKRecordConvertible {
+                    // Handle to-one relationship: https://realm.io/docs/swift/latest/#many-to-one
+                    // So the owner Object has to conform to CKRecordConvertible protocol
+                    r[prop.name] = CKRecord.Reference(recordID: owner.recordID, action: .none)
+                } else {
+                    /// Just a warm hint:
+                    /// When we set nil to the property of a CKRecord, that record's property will be hidden in the CloudKit Dashboard
+                    r[prop.name] = nil
+                }
+            default:
+                break
+            }
+            
+        }
+        return r
+    }
+    
+}
---
+++ Equalife/Pods/IceCream/IceCream/Classes/CreamAsset.swift
@@ -0,0 +1,201 @@
+//
+//  CreamAsset.swift
+//  IceCream
+//
+//  Created by Fu Yuan on 7/01/18.
+//
+
+import Foundation
+import RealmSwift
+import Realm
+import CloudKit
+
+/// If you want to store and sync big data automatically, then using CreamAsset might be a good choice.
+/// According to Apple https://developer.apple.com/documentation/cloudkit/ckasset :
+/// "You can also use assets in places where the data you want to assign to a field is more than a few kilobytes in size. "
+/// And According to Realm https://realm.io/docs/objc/latest/#current-limitations :
+/// "Data and String properties cannot hold data exceeding 16MB in size. To store larger amounts of data, either break it up into 16MB chunks or store it directly on the file system, storing paths to these files in the Realm. An exception will be thrown at runtime if your app attempts to store more than 16MB in a single property."
+/// We choose the latter, that's storing it directly on the file system, storing paths to these files in the Realm.
+/// So this is the deal.
+public class CreamAsset: Object {
+    @objc dynamic private var uniqueFileName = ""
+    override public static func ignoredProperties() -> [String] {
+        return ["filePath"]
+    }
+
+    private convenience init(objectID: String, propName: String) {
+        self.init()
+        self.uniqueFileName = "\(objectID)_\(propName)"
+    }
+    
+    /// Use this method to fetch the underlying data of the CreamAsset
+    public func storedData() -> Data? {
+        return try? Data(contentsOf: filePath)
+    }
+
+    /// Where the asset locates in the file system
+    public var filePath: URL {
+        return CreamAsset.creamAssetDefaultURL().appendingPathComponent(uniqueFileName)
+    }
+
+    /// Save the given data to local file system
+    /// - Parameters:
+    ///   - data: The data to save
+    ///   - path:
+    ///   - shouldOverwrite: Whether should overwrite current file existed at path or not.
+    private static func save(data: Data, to path: String, shouldOverwrite: Bool) throws {
+        let url = CreamAsset.creamAssetDefaultURL().appendingPathComponent(path)
+        guard shouldOverwrite || !FileManager.default.fileExists(atPath: url.path) else { return }
+        try data.write(to: url)
+    }
+
+    // MARK: - CKRecordConvertible & CKRecordRecoverable
+    
+    /// Wrap asset as CKAsset for uploading to CloudKit
+    var asset: CKAsset {
+        get {
+            return CKAsset(fileURL: filePath)
+        }
+    }
+
+    /// Parses a CKRecord and CKAsset back into a CreamAsset
+    ///
+    /// - Parameters:
+    ///   - propName: The unique property name to identify this asset. e.g.: Dog Object may have multiple CreamAsset properties, so we need unique `propName`s to identify these.
+    ///   - record: The CKRecord where we will pull the record ID off of to locate/store the file
+    ///   - asset: The CKAsset where we will pull the URL for creating the asset
+    /// - Returns: A CreamAsset if it was successful
+    static func parse(from propName: String, record: CKRecord, asset: CKAsset) -> CreamAsset? {
+        guard let url = asset.fileURL else { return nil }
+        return CreamAsset.create(objectID: record.recordID.recordName,
+                                 propName: propName,
+                                 url: url,
+                                 shouldOverwrite: true)
+    }
+
+    // MARK: - Factory methods
+    
+    /// Creates a new CreamAsset for the given object id with Data
+    ///
+    /// - Parameters:
+    ///   - objectID: The objectID (key property of the Realm object) the asset will be identified by
+    ///   - propName: The unique property name to identify this asset. e.g.: Dog Object may have multiple CreamAsset properties, so we need unique `propName`s to identify these.
+    ///   - data: The file data
+    ///   - shouldOverwrite: Whether to try and save the file even if an existing file exists for the same object ID.
+    /// - Returns: A CreamAsset if it was successful
+    public static func create(objectID: String, propName: String, data: Data, shouldOverwrite: Bool = true) -> CreamAsset? {
+        let creamAsset = CreamAsset(objectID: objectID,
+                                    propName: propName)
+        do {
+            try save(data: data, to: creamAsset.uniqueFileName, shouldOverwrite: shouldOverwrite)
+            return creamAsset
+        } catch {
+            // Os.log error here
+            return nil
+        }
+    }
+    
+    /// Creates a new CreamAsset for the given object with Data
+    ///
+    /// - Parameters:
+    ///   - object: The object the asset will live on
+    ///   - propName: The unique property name to identify this asset. e.g.: Dog Object may have multiple CreamAsset properties, so we need unique `propName`s to identify these.
+    ///   - data: The file data
+    ///   - shouldOverwrite: Whether to try and save the file even if an existing file exists for the same object.
+    /// - Returns: A CreamAsset if it was successful
+    public static func create(object: CKRecordConvertible, propName: String, data: Data, shouldOverwrite: Bool = true) -> CreamAsset? {
+        return create(objectID: object.recordID.recordName,
+                      propName: propName,
+                      data: data,
+                      shouldOverwrite: shouldOverwrite)
+    }
+
+    /// Creates a new CreamAsset for the given object with a URL
+    ///
+    /// - Parameters:
+    ///   - object: The object the asset will live on
+    ///   - propName: The unique property name to identify this asset. e.g.: Dog Object may have multiple CreamAsset properties, so we need unique `propName`s to identify these.
+    ///   - url: The URL where the file located
+    ///   - shouldOverwrite: Whether to try and save the file even if an existing file exists for the same object.
+    /// - Returns: A CreamAsset if it was successful
+    public static func create(object: CKRecordConvertible, propName: String, url: URL, shouldOverwrite: Bool = true) -> CreamAsset? {
+        return create(objectID: object.recordID.recordName,
+                      propName: propName,
+                      url: url,
+                      shouldOverwrite: shouldOverwrite)
+    }
+    
+    
+    /// Creates a new CreamAsset for the given objectID with a URL where asset locates
+    /// - Parameters:
+    ///   - objectID: The key to identify the object. Normally it's the recordName property of CKRecord.ID when recovering from CloudKit
+    ///   - propName: The unique property name to identify this asset. e.g.: Dog Object may have multiple CreamAsset properties, so we need unique `propName`s to identify these.
+    ///   - url: The location where asset locates
+    ///   - shouldOverwrite: Whether to try and save the file even if an existing file exists for the same object.
+    /// - Returns: The CreamAsset if creates successful
+    public static func create(objectID: String, propName: String, url: URL, shouldOverwrite: Bool = true) -> CreamAsset? {
+        let creamAsset = CreamAsset(objectID: objectID, propName: propName)
+        if shouldOverwrite {
+            do {
+                try FileManager.default.removeItem(at: creamAsset.filePath)
+            } catch {
+                // Os.log remove item failed error here
+            }
+        }
+        if !FileManager.default.fileExists(atPath: creamAsset.filePath.path) {
+            do {
+                try FileManager.default.copyItem(at: url, to: creamAsset.filePath)
+            } catch {
+                /// Os.log copy item failed
+                return nil
+            }
+        }
+        return creamAsset
+    }
+}
+
+extension CreamAsset {
+    /// The default path for the storing of CreamAsset. That is:
+    /// xxx/Document/CreamAsset/
+    public static func creamAssetDefaultURL() -> URL {
+        let documentDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        let commonAssetPath = documentDir.appendingPathComponent(className())
+        if !FileManager.default.fileExists(atPath: commonAssetPath.path) {
+            do {
+                try FileManager.default.createDirectory(atPath: commonAssetPath.path, withIntermediateDirectories: false, attributes: nil)
+            } catch {
+                /// Log: create directory failed
+            }
+        }
+        return commonAssetPath
+    }
+
+    /// Fetch all CreamAsset files' path
+    public static func creamAssetFilesPaths() -> [String] {
+        do {
+            return try FileManager.default.contentsOfDirectory(atPath: CreamAsset.creamAssetDefaultURL().path)
+        } catch {
+            
+        }
+        return [String]()
+    }
+
+    /// Execute deletions
+    private static func excecuteDeletions(in filesNames: [String]) {
+        for fileName in filesNames {
+            let absolutePath = CreamAsset.creamAssetDefaultURL().appendingPathComponent(fileName).path
+            do {
+                try FileManager.default.removeItem(atPath: absolutePath)
+            } catch {
+                /// Log: remove item failed at given path
+            }
+        }
+    }
+
+    /// When delete an object. We need to delete related CreamAsset files
+    public static func deleteCreamAssetFile(with id: String) {
+        let needToDeleteCacheFiles = creamAssetFilesPaths().filter { $0.contains(id) }
+        excecuteDeletions(in: needToDeleteCacheFiles)
+    }
+
+}
