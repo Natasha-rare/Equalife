e99880b60266da06db2e4d79c94a4377b2895ca1
Kostya Bunsberry <kostyabunsberry@macbook.local>
2021-07-31 17:44:06 +0700

    Тестовая синхронка, еще надо кое-что сделать, чтобы заработало

--- Equalife/Equalife/AppDelegate.swift
+++ Equalife/Equalife/AppDelegate.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import RealmSwift
+import CloudKit
+import IceCream
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var syncEngine: SyncEngine?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        syncEngine = SyncEngine(objects: [
+            SyncObject(type: RealmArticle.self),
+            SyncObject(type: RealmEditor.self)
+            ])
+        
+        application.registerForRemoteNotifications()
+//
+//        syncEngine?.pull(completionHandler: { error in
+//            UsersData.shared.didAlreadyGoToMain = true
+//            UsersData.shared.haveAlreadyLaunched = true
+//        })
+//
         return true
     }
 
@@ -29,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//        syncEngine?.pushAll()
     }
 
 
--- Equalife/Equalife/Model/News.swift
+++ Equalife/Equalife/Model/News.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RealmSwift
+import IceCream
 
 // Структура статьи (просто для начала)
 struct Article {
@@ -35,6 +36,46 @@ struct Article {
         }
     }
 }
+
+class RealmArticle: Object {
+    @objc dynamic var id = NSUUID().uuidString
+    @objc dynamic var title: String = ""
+    @objc dynamic var contents: String = ""
+    @objc dynamic var imagesURL: [String] = []
+    @objc dynamic var author: String? = ""
+    @objc dynamic var date: String = ""
+    @objc dynamic var isSaved: Bool = false
+    @objc dynamic var isDeleted: Bool = false
+    
+    static let AVATAR_KEY = "article"
+    @objc dynamic var avatar: CreamAsset?
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+    
+    convenience init(title: String, contents: String, imagesURL: [String], author: String?, date: String, isSaved: Bool) {
+        self.init()
+        self.title = title
+        self.contents = contents
+        
+        if let author = author {
+            self.author = author
+        }
+        
+        self.date = date
+        self.isSaved = isSaved
+        
+        for imageURL in imagesURL {
+            self.imagesURL.append(imageURL)
+        }
+    }
+}
+
+extension RealmArticle: CKRecordConvertible & CKRecordRecoverable {
+    // Leave it blank is all
+}
+
 
 enum EditorCategory: String {
     case politics
@@ -69,6 +110,7 @@ struct Editor {
 }
 
 class RealmEditor: Object {
+    @objc dynamic var id = NSUUID().uuidString
     @objc dynamic var name: String = ""
     @objc dynamic var imageName: String = ""
     @objc dynamic var info: String = ""
@@ -76,6 +118,14 @@ class RealmEditor: Object {
     @objc dynamic var sortId: Int = 0
     @objc dynamic var isAdded: Bool = false
     @objc dynamic var category: String = ""
+    @objc dynamic var isDeleted = false
+    
+    static let AVATAR_KEY = "editor"
+    @objc dynamic var avatar: CreamAsset?
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
     
     convenience init(name: String, imageName: String, info: String, editorId: Int, isAdded: Bool, category: [EditorCategory]) {
         self.init()
@@ -91,5 +141,6 @@ class RealmEditor: Object {
     }
 }
 
-
-// TODO: Здесь будут все добавленные издания, сохраненные в кэше, но это все потом
+extension RealmEditor: CKRecordConvertible & CKRecordRecoverable {
+    // Leave it blank is all
+}
